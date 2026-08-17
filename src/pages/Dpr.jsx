958f8d56f4e5f948927f030c2bab62367f122443
studio715 <studio@dipprojects.com>
2026-08-17 11:07:46 +0530

    updated code

--- src/pages/Dpr.jsx
+++ src/pages/Dpr.jsx
@@ -5006,7 +5006,7 @@ async function uploadBatch(items, uploadFn, concurrency = 4) {
 
         <div className="fg">
           <label className="flabel">
-            Site / Project <span className="req">*</span>
+            Site <span className="req">*</span>
           </label>
           {loadingSites ? (
             <input className="finput" value="Loading…" readOnly />
